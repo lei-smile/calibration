c19bdf9d246914c08c789372ca8d9abd9ec235b5
lei.wu <15850650098@163.com>
2021-04-02 13:42:39 +0800

    静态毫米波标定代码初版

---
+++ build/CMakeFiles/GPStoXYZ.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GPStoXYZ.dir/src/GPStoXYZ.cpp.o"
+  "GPStoXYZ.pdb"
+  "GPStoXYZ"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GPStoXYZ.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
