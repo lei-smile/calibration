982c224bf17b03bfe000a3d48f86eb0fe92095f7
lei.wu <15850650098@163.com>
2021-06-07 11:01:27 +0800

    相机与地面的单应性矩阵标定以及多相机的交叉标定验证

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -71,6 +71,6 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/radar_static_calib.dir/DependInfo.cmake"
-  "CMakeFiles/radar_static_calib_yaw.dir/DependInfo.cmake"
+  "CMakeFiles/cam_calib.dir/DependInfo.cmake"
+  "CMakeFiles/check_cam_calib.dir/DependInfo.cmake"
   )
---
+++ build/CMakeFiles/cam_calib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cam_calib.dir/src/cam2RTK_calib.cpp.o"
+  "cam_calib.pdb"
+  "cam_calib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cam_calib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
